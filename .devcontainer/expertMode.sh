a1ffbb7147a495f0aa4c34081651626b550758bc
Tim Park <timfpark@gmail.com>
2021-10-08 10:58:29 -0700

    Remove obsolete scripts

--- .devcontainer/expertMode.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools --force
-octant --disable-origin-check
--- start-krustlet.sh
+++
@@ -1,6 +0,0 @@
-bash <(curl https://raw.githubusercontent.com/krustlet/krustlet/main/scripts/bootstrap.sh)
-
-export IP=`ifconfig eth0 | awk '/inet / {print $2}'`
-KUBECONFIG=~/.kube/config KRUSTLET_NODE_NAME=krustlet bin/krustlet-wasi --node-ip $IP --hostname $IP --insecure-registries localhost:12345 --port 3000 --bootstrap-file ~/.krustlet/config/bootstrap.conf > bin/krustlet.out &
-sleep 2 
-kubectl certificate approve $IP-tls
