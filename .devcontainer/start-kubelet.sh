7c24d81faa9d857ae70c44faf496a59e029dea1e
Tim Park <timfpark@gmail.com>
2021-10-08 19:31:55 +0000

    Remove nohup

--- .devcontainer/start-kubelet.sh
+++ .devcontainer/start-kubelet.sh
@@ -4,4 +4,4 @@ bash <(curl https://raw.githubusercontent.com/krustlet/krustlet/main/scripts/boo
 
 export IP=`ifconfig eth0 | awk '/inet / {print $2}'`
 sleep 5 && kubectl certificate approve $IP-tls &
-nohup KUBECONFIG=~/.kube/config KRUSTLET_NODE_NAME=krustlet bin/krustlet-wasi --node-ip $IP --hostname $IP --insecure-registries localhost:12345 --port 3000 --bootstrap-file ~/.krustlet/config/bootstrap.conf > bin/krustlet.out &
+KUBECONFIG=~/.kube/config KRUSTLET_NODE_NAME=krustlet bin/krustlet-wasi --node-ip $IP --hostname $IP --insecure-registries localhost:12345 --port 3000 --bootstrap-file ~/.krustlet/config/bootstrap.conf
