1e2e99e90b79f08ae401090542da408800e5fdbc
Tim Park <timfpark@gmail.com>
2021-10-08 19:17:26 +0000

    Fix 2nd deployment issue

--- .devcontainer/run.sh
+++ .devcontainer/run.sh
@@ -4,7 +4,14 @@ set -euo pipefail
 export TAG=`date -u +"%Y%m%dT%H%M%SZ"`
 
 cargo build --target wasm32-wasi --release
-bin/wasm-to-oci push target/wasm32-wasi/release/hello-world-rust.wasm "localhost:12345/hello-world-rust:${TAG}"
+wasm-opt -O4 target/wasm32-wasi/release/hello-world-rust.wasm -o target/wasm32-wasi/release/hello-world-rust-opt.wasm
+bin/wasm-to-oci push target/wasm32-wasi/release/hello-world-rust-opt.wasm "localhost:12345/hello-world-rust:${TAG}"
+
+DEPLOYMENTS=`kubectl get deployments | wc -l`
+if [ "$DEPLOYMENTS" -ge 2 ]
+then
+    kubectl delete deployment hello-world-wasi-rust
+fi
 
 cat deploy/codespace.yaml | envsubst | kubectl apply -f -
 
